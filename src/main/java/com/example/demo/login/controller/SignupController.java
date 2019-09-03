69a09873d546ea0d98ef079dac590d46d5ca3d2b
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-03 05:43:31 +0000

    コントローラーを8章終了時点に更新

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -3,13 +3,25 @@ package com.example.demo.login.controller;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.GroupOrder;
+import com.example.demo.login.domain.model.SignupForm;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.service.UserService;
 
 @Controller
 public class SignupController {
+
+    @Autowired
+    private UserService userService;
 
     //ラジオボタン用変数
     private Map<String, String> radioMarriage;
@@ -32,7 +44,7 @@ public class SignupController {
      * ユーザー登録画面のGETメソッド用処理.
      */
     @GetMapping("/signup")
-    public String getSignUp(Model model) {
+    public String getSignUp(@ModelAttribute SignupForm form, Model model) {
 
         // ラジオボタンの初期化メソッド呼び出し
         radioMarriage = initRadioMarrige();
@@ -48,7 +60,41 @@ public class SignupController {
      * ユーザー登録画面のPOSTメソッド用処理.
      */
     @PostMapping("/signup")
-    public String postSignUp(Model model) {
+    public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form,
+            BindingResult bindingResult,
+            Model model) {
+
+        // 入力チェックに引っかかった場合、ユーザー登録画面に戻る
+        if (bindingResult.hasErrors()) {
+
+            // GETリクエスト用のメソッドを呼び出して、ユーザー登録画面に戻ります
+            return getSignUp(form, model);
+
+        }
+
+        // formの中身をコンソールに出して確認します
+        System.out.println(form);
+
+        // insert用変数
+        User user = new User();
+
+        user.setUserId(form.getUserId()); //ユーザーID
+        user.setPassword(form.getPassword()); //パスワード
+        user.setUserName(form.getUserName()); //ユーザー名
+        user.setBirthday(form.getBirthday()); //誕生日
+        user.setAge(form.getAge()); //年齢
+        user.setMarriage(form.isMarriage()); //結婚ステータス
+        user.setRole("ROLE_GENERAL"); //ロール（一般）
+
+        // ユーザー登録処理
+        boolean result = userService.insert(user);
+
+        // ユーザー登録結果の判定
+        if (result == true) {
+            System.out.println("insert成功");
+        } else {
+            System.out.println("insert失敗");
+        }
 
         // login.htmlにリダイレクト
         return "redirect:/login";
